a7345284b4e85a38ada281ab8f4892713b6fd99c
Nicole M Darby <nicoledarby021322@gmail.com>
2019-10-21 01:36:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,7 @@
-#write your code here
-
-def countdown
+def countdown output
+  puts=["10 seconds", "9 seconds", "8 seocnds", "7 seconds", "6 seconds", "5 seconds", "4 seconds", "3 seconds", "2 seconds", "1 seconds"]
 end
+
+countdown_to_10=0 
+
+while number_of_seconds
